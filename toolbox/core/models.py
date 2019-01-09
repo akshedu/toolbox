e6b9fe5ed325c19afd0c6a733ee29ed886dd935d
Akshansh Gupta <akshedu@gmail.com>
2019-01-09 21:24:38 +0530

    Refactor Models, Add thumbnails

--- toolbox/core/models.py
+++ toolbox/core/models.py
@@ -1,14 +1,31 @@
 from django.db import models
 
 # Create your models here.
-class Channel(models.Model):
-	channel = models.OneToOneField('scraper.TrackedChannel', on_delete=models.CASCADE, primary_key=True)
-	title = models.CharField(max_length=255)
+class Thumbnail(models.Model):
+	default_url = models.TextField()
+	medium_url = models.TextField()
+	high_url = models.TextField()
+
+
+class Description(models.Model):
 	description = models.TextField()
+
+
+class Resource(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.ForeignKey(Description, on_delete=models.CASCADE)
 	published_at = models.DateTimeField()
-	country = models.CharField(max_length=3,null=True, blank=True)
+	thumbnail = models.ForeignKey(Thumbnail, on_delete=models.CASCADE)
 	last_updated = models.DateTimeField(auto_now_add=True)
 
+	class Meta:
+		abstract = True
+
+
+class Channel(Resource):
+	channel = models.OneToOneField('scraper.TrackedChannel', on_delete=models.CASCADE, primary_key=True)
+	country = models.CharField(max_length=3, null=True, blank=True)
+	
 
 class ChannelVideoMap(models.Model):
 	channel = models.ForeignKey(Channel, to_field='channel_id', on_delete=models.CASCADE)
@@ -18,13 +35,10 @@ class ChannelVideoMap(models.Model):
 		unique_together = ('channel', 'video_id',)
 
 
-class Video(models.Model):
+class Video(Resource):
 	video = models.OneToOneField(ChannelVideoMap, to_field='video_id', on_delete=models.CASCADE, primary_key=True)
-	title = models.CharField(max_length=255)
 	duration = models.IntegerField()
 	keywords = models.TextField()
-	published_date = models.DateTimeField()
-	last_updated = models.DateTimeField(auto_now_add=True)
 
 
 class ChannelStats(models.Model):
@@ -43,8 +57,10 @@ class VideoStats(models.Model):
 	video = models.ForeignKey(Video, on_delete=models.CASCADE)
 	views = models.BigIntegerField()
 	likes = models.BigIntegerField()
+	dislikes = models.BigIntegerField()
+	favorites = models.BigIntegerField()
 	comments = models.BigIntegerField()
-	crawled_date = models.DateTimeField()
+	crawled_date = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
 		unique_together = ('video', 'crawled_date',)
