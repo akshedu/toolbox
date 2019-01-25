8d40428c6a01a4f6618ad6963c057fe39b892167
Garvit Verma <garvit@bizongo.in>
2019-01-25 13:02:40 +0530

    Added angular dependencies in bower json

---
+++ toolbox/static/js/angular-app/app.js
@@ -0,0 +1,11 @@
+var toolboxApp;
+toolboxApp = angular.module(
+  'toolboxApp', [
+    'ngResource',
+    'templates',
+    'ui.router',
+    'ui.bootstrap',
+    'ngCookies',
+    'base64',
+  ]
+)
