23957153b08386938b8be1eb3c86132ea87be7fa
Akshansh Gupta <akshedu@gmail.com>
2019-03-11 00:03:23 +0530

    Capture Integrity error

--- toolbox/core/management/commands/celery_beat_resource_scraper.py
+++ toolbox/core/management/commands/celery_beat_resource_scraper.py
@@ -1,28 +1,43 @@
 
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        schedule_channel, created = IntervalSchedule.objects.get_or_create(
-                                every=4,
-                                period=IntervalSchedule.HOURS,
-                            )
-        schedule_video, created = IntervalSchedule.objects.get_or_create(
-                                every=6,
-                                period=IntervalSchedule.HOURS,
-                            )
-        PeriodicTask.objects.create(
-            interval=schedule_channel,                  
-            name='Scrape Channels',          
-            task='toolbox.scraper.scrape_youtube_channels',  
-        )
+        try:
+            schedule_channel, created = IntervalSchedule.objects.get_or_create(
+                                    every=4,
+                                    period=IntervalSchedule.HOURS,
+                                )
+        except IntegrityError as e:
+            pass
 
-        PeriodicTask.objects.create(
-            interval=schedule_video,                  
-            name='Scrape Videos',          
-            task='toolbox.scraper.scrape_youtube_videos',  
-        )
+        try:
+            schedule_video, created = IntervalSchedule.objects.get_or_create(
+                                    every=6,
+                                    period=IntervalSchedule.HOURS,
+                                )
+        except IntegrityError as e:
+            pass
+
+        try:
+            PeriodicTask.objects.create(
+                interval=schedule_channel,                  
+                name='Scrape Channels',          
+                task='toolbox.scraper.scrape_youtube_channels',  
+            )
+        except IntegrityError as e:
+            pass
+
+        try:
+            PeriodicTask.objects.create(
+                interval=schedule_video,                  
+                name='Scrape Videos',          
+                task='toolbox.scraper.scrape_youtube_videos',  
+            )
+        except IntegrityError as e:
+            pass
 
 
--- toolbox/core/management/commands/celery_beat_top_videos.py
+++ toolbox/core/management/commands/celery_beat_top_videos.py
@@ -1,24 +1,35 @@
 
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        schedule, _ = CrontabSchedule.objects.get_or_create(
-            minute='0',
-            hour='23',
-            day_of_week='*',
-            day_of_month='*',
-            month_of_year='*',)
-        PeriodicTask.objects.create(
-            crontab=schedule,
-            name='Top Videos',
-            task='toolbox.core.create_top_videos',
-        )
+        try:
+            schedule, _ = CrontabSchedule.objects.get_or_create(
+                minute='0',
+                hour='23',
+                day_of_week='*',
+                day_of_month='*',
+                month_of_year='*',)
+        except IntegrityError as e:
+            pass
 
-        PeriodicTask.objects.create(
-            crontab=schedule,
-            name='Top Channels',
-            task='toolbox.core.create_top_channels',
-        )
+        try:
+            PeriodicTask.objects.create(
+                crontab=schedule,
+                name='Top Videos',
+                task='toolbox.core.create_top_videos',
+            )
+        except IntegrityError as e:
+            pass
+
+        try:
+            PeriodicTask.objects.create(
+                crontab=schedule,
+                name='Top Channels',
+                task='toolbox.core.create_top_channels',
+            )
+        except IntegrityError as e:
+            pass
