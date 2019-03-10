6d2228162e60d62bf88a395edb0eb820bffdcd33
Akshansh Gupta <akshedu@gmail.com>
2019-03-11 00:41:39 +0530

    Shift schedule to web

--- docker-entrypoint-celery-beat.sh
+++ docker-entrypoint-celery-beat.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Schedule scrapers"
-python manage.py celery_beat_resource_scraper &
-python manage.py celery_beat_top_videos
-
 echo "start beat service"
 celery -A config beat -l info -f beat.logs --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- docker-entrypoint-web.sh
+++ docker-entrypoint-web.sh
@@ -4,5 +4,9 @@ set -e
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Schedule scrapers"
+python manage.py celery_beat_resource_scraper &
+python manage.py celery_beat_top_videos
+
 echo "runserver"
 python manage.py runserver 0.0.0.0:8000
