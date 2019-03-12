d9136ea0006596d363addaf22f36274bd3c9dda8
Akshansh Gupta <akshedu@gmail.com>
2019-03-12 23:46:47 +0530

    Prod ready

--- docker-entrypoint-web.sh
+++ docker-entrypoint-web.sh
@@ -8,5 +8,7 @@ echo "Schedule scrapers"
 python manage.py celery_beat_resource_scraper &
 python manage.py celery_beat_top_videos
 
-echo "runserver"
-python manage.py runserver 0.0.0.0:8000
+echo "Starting gunicorn"
+gunicorn config.wsgi:application --certfile keys/cert.pem \
+    --keyfile keys/key.pem --bind 0.0.0.0:8000 --workers 4 \
+    --timeout 308000
